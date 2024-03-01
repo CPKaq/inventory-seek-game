4353c77d1928d9c5800084a9ae57694ae7989c94
CPK <CPKbaidu@163.com>
2024-03-01 12:11:49 +0800

    update level.js

--- level.js
+++ level.js
@@ -1,6 +1,10 @@
 level = [
     ['Copper Ingot', 'Brick'],
     ['Bamboo', 'Stick'],
+    ['Clay Ball', 'Snowball'],
+    ['Block of Diamond', 'Light Blue Wool'],
+    ['Coal', 'Flint'],
+    ['Calcite', 'Snow Block'],
     ['Granite', 'Dirt'],
     ['Block of Coal', 'Black Concrete']
 ]
