714572ddf1da9b32afff1a3a9b3dd7b6e3b5ff00
CPK <CPKbaidu@163.com>
2024-03-03 21:49:05 +0800

    update level.js

--- level.js
+++ level.js
@@ -5,6 +5,9 @@ level = [
     ['Block of Diamond', 'Light Blue Wool', '钻石块', '淡蓝色羊毛'],
     ['Coal', 'Flint', '煤炭', '燧石'],
     ['Calcite', 'Snow Block', '方解石', '雪块'],
+    ['Block of Bamboo', 'Melon', '竹块', '西瓜'],
     ['Granite', 'Dirt', '花岗岩', '泥土'],
+    ['Horn Coral Block', 'Sponge', '鹿角珊瑚块', '海绵'],
+    ['Green Concrete Powder', 'Moss Block', '绿色混凝土粉末', '苔藓块'],
     ['Block of Coal', 'Black Concrete', '煤炭块', '黑色混凝土']
 ]
